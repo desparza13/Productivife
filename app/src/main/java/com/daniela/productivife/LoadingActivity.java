3c77bd03646d6267f02d905ebe5baf91a7bab30a
Daniela Esparza Espinosa <desparza13@fb.com>
2022-06-20 10:37:51 -0700

    "Created loading animation launched when the app is first launched, supported by Lottie's animation"

---
+++ app/src/main/java/com/daniela/productivife/LoadingActivity.java
@@ -0,0 +1,25 @@
+package com.daniela.productivife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        int time = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+                finish();
+            }
+        }, time);
+    }
+}

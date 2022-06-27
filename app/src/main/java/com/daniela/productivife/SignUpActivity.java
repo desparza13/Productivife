802f3051ded0b71ec43dafc3bb5d35540746ce09
Daniela Esparza <56266109+desparza13@users.noreply.github.com>
2022-06-27 11:39:04 -0700

    Facebook login
    Tried to include Facebook SDK login but ran into some issues, WIP

--- app/src/main/java/com/daniela/productivife/SignUpActivity.java
+++ app/src/main/java/com/daniela/productivife/SignUpActivity.java
@@ -7,12 +7,22 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
@@ -20,11 +30,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
+    public static final String TAG = "SignUp";
     private Button btnLogin;
     private Button btnSignup;
+    private LoginButton btnFacebook;
     private EditText etName;
     private EditText etEmail;
     private EditText etPassword;
@@ -38,13 +53,19 @@ public class SignUpActivity extends AppCompatActivity {
     private String password = "";
     private String confirmPassword = "";
 
+    private CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        callbackManager = CallbackManager.Factory.create();
+
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignup);
+        btnFacebook = findViewById(R.id.btnFacebook);
         etName = findViewById(R.id.etName);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
@@ -54,6 +75,29 @@ public class SignUpActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(SignUpActivity.this);
         progressDialog.setTitle("Wait a second please");
         progressDialog.setCanceledOnTouchOutside(false); //User can't close de process dialog when pressing
+
+        btnFacebook = (LoginButton) findViewById(R.id.btnFacebook);
+        btnFacebook.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(SignUpActivity.this, "Log in through facebook successful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(SignUpActivity.this, "Log in through facebook cancelled", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(SignUpActivity.this, "Log in through facebook error"+exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +112,12 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     private void checkData(){
         name = etName.getText().toString();
         email = etEmail.getText().toString();
